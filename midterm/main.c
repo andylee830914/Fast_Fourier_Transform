d2c0589627901e2854b70e324b1b0a13bb0f7d28
Yu Hsun Lee <andylee0914.tw@gmail.com>
2015-04-15 11:32:50 +0800

    initial

---
+++ midterm/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  midterm
+//
+//  Created by Yu Hsun Lee on 2015/4/15.
+//  Copyright (c) 2015年 Yu Hsun Lee. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
